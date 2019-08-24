4cc207473d057ff8e24f61d9c83935a09345609a
Magnus Lindhult <magnus.lindhult@gmail.com>
2019-08-24 13:04:51 +0200

    More function descriptions

--- src/randomizationService.js
+++ src/randomizationService.js
@@ -1,7 +1,17 @@
 const debug = require("debug")("randomizationService");
 
+/**
+ * Given a number, this function will pick a new random number between 0 and the input.
+ *
+ * @param {*} arrayLength
+ */
 const pickRandomIndex = arrayLength => Math.floor(Math.random() * arrayLength);
 
+/**
+ * Given a list of exerciseModels, this function will pick one random exerciseModel and return it.
+ *
+ * @param {*} exerciseModels
+ */
 const pickRandomExercise = exerciseModels => {
   const randomIndex = pickRandomIndex(exerciseModels.length);
   debug(`Picking an exercise with a random index of ${randomIndex}`);
